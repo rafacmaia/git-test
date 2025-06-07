4253092d1e99a0050002b42c417d6bdc3a87232f
RCMaia <rfpcmaia@hotmail.com>
2025-06-07 13:11:57 -0400

    Version 1 test

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Zouzou!");
+        System.out.print("Hi Zou!\n");
+        System.out.println("Hi Zou #2!");
+
+        int age;
+        // System.out.println(age); // this will crash the program
+
+        age = 11;
+        System.out.println(age);
+    }
+
+}
+
+// 21:20
