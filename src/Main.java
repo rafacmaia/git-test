8b0596760b60ffda6ed31627711d42cba18cc67d
RCMaia <rfpcmaia@hotmail.com>
2025-06-15 13:49:02 -0400

    User input

--- src/Main.java
+++ src/Main.java
@@ -8,20 +8,29 @@ public class Main {
         System.out.print("Hi Zou!\n");
         System.out.println("Hi Zou #2!");
 
-        int age;
+        int zouAge;
         // System.out.println(age); // this will crash the program
 
-        age = 11;
-        System.out.println(age);
+        zouAge = 11;
+        System.out.println(zouAge);
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the Queen's name: ");
-        scanner.nextLine();
+        System.out.print("Enter the Queen's name: ");
+        String queen = scanner.nextLine();
+        System.out.print("How old is she? ");
+        int age = scanner.nextInt();
+
+        if (queen.equals("Zouzou") || queen.equals("Zou")) {
+            System.out.println("Correct, she is indeed the queen.");
+            System.out.println("Long live The " + queen + "!");
+        } else {
+            System.out.println("Incorrect, shame on you and your lineage.");
+            System.out.println("Take your '" + queen + "' guess out of here.");
+        }
+        System.out.println("She's " + age + " glorious years old.");
 
         scanner.close();
     }
-
+// 36:20
 }
-
-// 21:20
