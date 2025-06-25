e3ef4420b837004a96c080f8b0e17469c334f0f0
RCMaia <rfpcmaia@hotmail.com>
2025-06-25 15:56:20 -0400

    Version 7

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Zouzou!");
-        System.out.print("Hi Zou!\n");
-        System.out.println("Hi Zou #2!");
-
-        int zouAge;
-        // System.out.println(age); // this will crash the program
-
-        zouAge = 11;
-        System.out.println(zouAge);
+        double width = 0, height = 0, area = 0;
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the Queen's name: ");
-        String queen = scanner.nextLine();
-        System.out.print("How old is she? ");
-        int age = scanner.nextInt();
+        System.out.print("Enter the width of the rectangle: ");
+        width = scanner.nextDouble();
 
-        if (queen.equals("Zouzou") || queen.equals("Zou")) {
-            System.out.println("Correct, she is indeed the queen.");
-            System.out.println("Long live The " + queen + "!");
-        } else {
-            System.out.println("Incorrect, shame on you and your lineage.");
-            System.out.println("Take your '" + queen + "' guess out of here.");
-        }
-        System.out.println("She's " + age + " glorious years old.");
+        System.out.print("Enter the height of the rectangle: ");
+        height = scanner.nextDouble();
+
+        area = width * height;
+
+        System.out.println("The area of the rectangle is " + area + "cm²");
 
         scanner.close();
     }
-// 37:35
+// 45:45
 }
